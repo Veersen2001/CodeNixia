9e4c14dcb0c33a3ced5110f5dc5ec156f62f1c6c
veersen2001 <veerbahadursen@gmail.com>
2023-12-15 22:27:33 +0530

    project

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 // Third-Party
 app.use(
   cors({
-    origin: ["https://code-nixia-irwx.vercel.app./"], //5000
+    origin: [process.env.FRONTEND_URL], //5000
     credentials: true,
   })
 )
